a238bade776068b649105034591b6d5434a56b6d
JasierDeen <jasierdeen70@gmail.com>
2024-10-19 17:25:10 +0530

    Initial commit of local code

---
+++ todo_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Todo
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'memo', 'important']
+        # widgets = {
+        #     'title': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'memo': forms.Textarea(attrs={'class': 'form-control'}),
+        #     'important': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        # }
